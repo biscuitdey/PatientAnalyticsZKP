db5d2c350872d0b1ae562f8d218c52aa333fecb3
biscuitdey <biswashreedey@gmail.com>
2024-02-13 16:44:25 +0530

    Add main function for testing

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+
+	//TEST
+	//1. Calculate average age of patients (unencrypted)
+
+	//2. Calculate average age of patients (encrypted)
+	//2.1. Create ledger
+	//2.2. Create encryptedPatient
+	//2.3. Add encryptedPatient to ledger
+	//2.4. Get average get of patients with specific disease
+
+	//3. Open the commitment to verify --> unencrypted average age matches encrypted average age
+	// zksigma.Open(a, b, c, average)
+}
