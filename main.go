de2dd3cdb4e3a541af5e04e9835318547d9baf5f
biscuitdey <biswashreedey@gmail.com>
2024-02-14 07:37:30 +0530

    Remove unnecesary comments

--- main.go
+++ main.go
@@ -3,24 +3,7 @@ package main
 import (
 	"crypto/sha256"
 	"math/big"
-
-	"github.com/consensys/gnark/frontend"
 )
-
-type CubicCircuit struct {
-	// struct tags on a variable is optional
-	// default uses variable name and secret visibility.
-	X frontend.Variable `gnark:"x"`
-	Y frontend.Variable `gnark:",public"`
-}
-
-// Define declares the circuit constraints
-// x**3 + x + 5 == y
-func (circuit *CubicCircuit) Define(api frontend.API) error {
-	x3 := api.Mul(circuit.X, circuit.X, circuit.X)
-	api.AssertIsEqual(circuit.Y, api.Add(x3, circuit.X, 5))
-	return nil
-}
 
 func main() {
 
