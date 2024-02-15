3f980611ce7f026017bf2e14a5b4c6be688133a8
biscuitdey <biswashreedey@gmail.com>
2024-02-15 07:17:48 +0530

    Create patients with commitments and zkp

---
+++ patient.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"math/big"
+
+	"github.com/mit-dci/zksigma"
+)
+
+type AgeCommitment struct {
+	commitment  zksigma.ECPoint
+	randomValue big.Int
+}
+type EncryptedPatient struct {
+	id      int
+	disease ZeroKnowledgeProof
+	age     AgeCommitment
+}
+
+func MakeEncryptedPatient(disease string, age int) *EncryptedPatient {
+
+	diseaseZKP := generateDiseaseProof(disease)
+
+	ageCommitment := generateAgeCommitment(age)
+
+	encryptedPatient := &EncryptedPatient{
+		disease: *diseaseZKP,
+		age:     *ageCommitment,
+	}
+	return encryptedPatient
+}
+
+func generateDiseaseProof(disease string) *ZeroKnowledgeProof {
+	return generateProof(disease)
+}
+
+func generateAgeCommitment(age int) *AgeCommitment {
+	ageBigInt := new(big.Int).SetInt64(int64(age))
+	commitment, randomValue, _ := zksigma.PedCommit(PatientLedgerCurve, ageBigInt)
+	ageCommitment := &AgeCommitment{
+		commitment:  commitment,
+		randomValue: *randomValue,
+	}
+
+	return ageCommitment
+}
+
+func verifyAgeCommitment(age int, ageCommitment AgeCommitment) bool {
+	ageBigInt := new(big.Int).SetInt64(int64(age))
+	return zksigma.Open(PatientLedgerCurve, ageBigInt, &ageCommitment.randomValue, ageCommitment.commitment)
+}
